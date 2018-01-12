7abe4aab5d350b5f0874f342952bc97f2da66368
samoot1968 <Toomas.unt1968@gmail.com>
2018-01-12 15:50:54 +0200

    Testing

---
+++ env_config.php
@@ -0,0 +1,17 @@
+<?php
+ 
+return [
+    'settings' => [
+		'displayErrorDetails' => ,
+		'db' => [
+            'driver' => '',
+            'host' => '',
+            'database' => '',
+            'username' => '',
+            'password' => '',
+            'charset'   => '',
+            'collation' => '',
+            'prefix'    => '',
+        ]
+    ]
+];
